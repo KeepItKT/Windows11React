becc5ad7623cee36b7070b12665829d1a8fa9cfc
ImKTKota <104640736+ImKTKota@users.noreply.github.com>
2022-05-17 10:30:11 -0400

    Update dycalendar.js

--- public/dycalendar.js
+++ public/dycalendar.js
@@ -1,17 +1,4 @@
-/*!
- * dyCalendar is a JavaScript library for creating Calendar.
- *
- * Author: Yusuf Shakeel
- * https://github.com/yusufshakeel
- *
- * GitHub Link: https://github.com/yusufshakeel/dyCalendarJS
- *
- * MIT license
- * Copyright (c) 2016 Yusuf Shakeel
- *
- * Date: 2014-08-17 sunday
- */
-/*! dyCalendarJS | (c) 2016 Yusuf Shakeel | https://github.com/yusufshakeel/dyCalendarJS */
+
 (function(global) {
 
   "use strict";
